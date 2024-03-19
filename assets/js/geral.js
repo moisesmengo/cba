558583bf70be02d0bf03ed2e50b9eedc6561e431
Moisés <guile@studiobrasuka.com.br>
2024-03-19 16:08:37 -0300

    menus e paginas de notificação

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -10,3 +10,47 @@ document.addEventListener("DOMContentLoaded", () => {
     menu.classList.remove("ativo");
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".areta-total");
+  const menuList = document.querySelector(".notificacoes-menu");
+
+  const eventos = ["click"];
+
+  function openMenu(e) {
+    menuList.classList.add("active");
+    menuButton.classList.add("active");
+
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove("active");
+      menuButton.classList.remove("active");
+    });
+  }
+  eventos.forEach((userEvent) => {
+    menuButton.addEventListener(userEvent, openMenu);
+  });
+});
+
+function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
+
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      setTimeout(() => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      });
+    });
+    element.setAttribute(outside, "");
+  }
+
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
+    }
+  }
+}
